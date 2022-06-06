030ad68fab88345ef5d2ae482438abdab8ba3c15
alexG278 <alexgonza278fi@gmail.com>
2022-06-06 09:02:02 -0300

    etapa1

---
+++ app1.js
@@ -0,0 +1,9 @@
+function getValues() {
+    let inputNombre = document.getElementById("nombre").value;
+    let inputDinero = parseInt(document.getElementById("dinero").value)
+    console.log(inputNombre + ":" + "$" + inputDinero);
+}
+
+
+
+
